c63829756d5bc8d3052d3b986aef3d1839f456ac
a.w <andrewwayneisbored@gmail.com>
2021-11-05 13:33:03 -0700

    Updating code to reflect Giphy attribution marks

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var tag = 'cats' // Search Terms
 var api = 'Your API Key' // Your API Key
 var rating = 'pg' // Rating of the gifs
-var wid = 500 // image resolution width
-var hgt = 500 // image resolution height
+var wid = 640 // image resolution width
+var hgt = 480 // image resolution height
 
 var request_Url = 'https://api.giphy.com/v1/gifs/random?api_key=' + api + '&tag=' + tag + '&rating=' + rating
 
@@ -22,6 +22,11 @@ request.onload = function() {
 	gif.setAttribute("width",wid);
 	gif.setAttribute("height",hgt);
 	document.body.appendChild(gif);
+	const giphy = document.createElement('img');
+	giphy.src = "Poweredby_100px-Black_VertLogo.png"
+	giphy.style.marginLeft = '-100px';
+	giphy.style.marginBottom = '10px';
+	document.body.appendChild(giphy);
 };
 
 request.send();
